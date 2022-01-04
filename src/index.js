e41186e0392cfbc31346431f634ff018e67db492
Spin4ester <bakibayev.r@gmail.com>
2022-01-04 17:13:12 +0600

    feat: add number conversion function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function toReadable (number) {
+    const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen','fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
   
-}
+  
+      let numStr = number.toString();
+      
+      if(number === 0) return 'zero'
+
+      if(number < 20) return ones[number]
+  
+      if(numStr.length === 2) return (tens[numStr[0]] + ' ' + ones[numStr[1]]).trim()
+      
+      if(numStr.length === 3) 
+        if(numStr[1] === '0') return (ones[numStr[0]] + ' hundred ' + ones[numStr[2]]).trim()
+        else return (ones[numStr[0]] + ' hundred' + ' ' + toReadable(numStr[1] + numStr[2])).trim()
+
+  }
+  
+
+
